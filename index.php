848ea8275613611556ffcd779dd51602a984225b
Jerwen Kiev <77055176+mrjerwenkiev7@users.noreply.github.com>
2021-02-25 21:24:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,16 +26,16 @@
   <img src="MATH - PERFORMANCE TASK DESIGN.png", width="400"></img>
   <br><br><br>
 <p align="justify">
-  <font color="ADFF2F", size="4", face="Courier">
-    Losing opportunities, time, and chances discouraged us greatly. Losing someone we love was never easy. Losing things and jobs which sustain is will give us heavy burdens to carry. But losing hope will shatter our lives.
-    <br><br>
-    The pandemic may have dimmed all the lights that we were all trying to construct in hopes to see a brighter future, left us in pitch black, and devoured all the hope that we have, know that there will always be an intangible light inside you that only you can choose to dim... so have hope. In our artwork, you can see the cross at center and at the uppermost part because God is the intangible light- He is the one that will continue to shine within us in midst of the darkness, He is hope and trusting Him is the best way to see a bright side. 
-    <br><br>
-    The dove symbolizes the Holy Spirit and peace that knocks on each of our hearts and live there, that endures and persists to knock especially when the sky is gray and dark. When you let both to enter your hearts, you are letting God to enlighten you and you are Him use you to bear actions and bear hope for others, like what the growing leaves is signifying. Those actions intended to plant hope to someone will plant hope to the world as it spreads. 
-    <br><br>
-    COVID-19 pandemic affected the lives of many people, that's why through our artwork, we wanted relay the message, “Never lose hope.” Trust God for He is the center of everything, always let the Holy Spirit to bring you peace and hope and be your driving force to be a light to someone else. Hope is created at the intersections of faith, perseverance, and love. In this time of the pandemic, one of the greatest gifts you can give to those you love is the invitation to discover hope.
-    <br><br>
-    If we learn to lift everything and do the work by doing good. Great things will happen at the right time, at the right moment, at the right place, at the right person. In the middle of this pandemic, hope is important to persevere because it gives us courage to see the brighter side of every difficulties that come.  May this be a reminder for us to keep going no matter what we comes to hinder us. Let's be hopeful, always.
+  <font color="006400", size="4", face="Courier">
+          Losing opportunities, time, and chances discouraged us greatly. Losing someone we love was never easy. Losing things and jobs which sustain is will give us heavy burdens to carry. But losing hope will shatter our lives.
+          <br><br>
+          The pandemic may have dimmed all the lights that we were all trying to construct in hopes to see a brighter future, left us in pitch black, and devoured all the hope that we have, know that there will always be an intangible light inside you that only you can choose to dim... so have hope. In our artwork, you can see the cross at center and at the uppermost part because God is the intangible light- He is the one that will continue to shine within us in midst of the darkness, He is hope and trusting Him is the best way to see a bright side. 
+          <br><br>
+          The dove symbolizes the Holy Spirit and peace that knocks on each of our hearts and live there, that endures and persists to knock especially when the sky is gray and dark. When you let both to enter your hearts, you are letting God to enlighten you and you are Him use you to bear actions and bear hope for others, like what the growing leaves is signifying. Those actions intended to plant hope to someone will plant hope to the world as it spreads. 
+          <br><br>
+          COVID-19 pandemic affected the lives of many people, that's why through our artwork, we wanted relay the message, “Never lose hope.” Trust God for He is the center of everything, always let the Holy Spirit to bring you peace and hope and be your driving force to be a light to someone else. Hope is created at the intersections of faith, perseverance, and love. In this time of the pandemic, one of the greatest gifts you can give to those you love is the invitation to discover hope.
+          <br><br>
+          If we learn to lift everything and do the work by doing good. Great things will happen at the right time, at the right moment, at the right place, at the right person. In the middle of this pandemic, hope is important to persevere because it gives us courage to see the brighter side of every difficulties that come.  May this be a reminder for us to keep going no matter what we comes to hinder us. Let's be hopeful, always.
   </font>
   <br><br><br><br>
 <p align="center">
