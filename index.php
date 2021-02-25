46bd28125f971afefce7dcf6fa545e71554a0d56
Jerwen Kiev <77055176+mrjerwenkiev7@users.noreply.github.com>
2021-02-25 20:57:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,15 +12,15 @@
   <img src="week3/logo.png", width="210", length="210"></img>
   <br><br>
   <font color="006400", size="7", face="Courier"><b>Ecolution</b></font>
-  <br><br>
-  <font color="ADFF2F", size="2", face="Courier"><b>Lasallian Advocates for Mother Earth</b></font>
   <br>
-  <font color="ADFF2F", size="2", face="Courier"><b>Since 2021</b></font>
-  <br><br>
+  <font color="ADFF2F", size="3", face="Courier"><b>Lasallian Advocates for Mother Earth</b></font>
+  <br>
+  <font color="ADFF2F", size="3", face="Courier"><b>Since 2021</b></font>
+  <br><br><br>
   <font color="006400", size="5", face="Courier"><b>HOPE Video Advocacy</b></font>
   <br>
   <a href="https://youtu.be/MV4PNpAKGgc">
-    <font color="ADFF2F", size="5", face="Courier"><b><i>(Click here)</i></b></font>
+    <font color="ADFF2F", size="4", face="Courier"><b><i>(Click here)</i></b></font>
     </a>
   <br><br><br><br>
   <font color="006400",size="2", face="Courier"><b>A Group 8 Project in TLE 9</b></font>
