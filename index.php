74121c70a76c00c921501bb04335ef18725e01a0
Jerwen Kiev <77055176+mrjerwenkiev7@users.noreply.github.com>
2021-02-25 20:40:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <style> 
 body {background-image: url("week3/bg.jpg"); background-repeat: no-repeat; background-attachment: fixed; background-size: cover;}
 </style>
-<br><br><br><br><br>
+<br><br><br><br><br><br>
 <p align="center">
 <img src="week3/logo.png", width="210", length="210"></img>
 <br><br>
@@ -13,7 +13,7 @@ body {background-image: url("week3/bg.jpg"); background-repeat: no-repeat; backg
 <font color="ADFF2F",size="12", face="Courier"><b>Lasallian Advocates for Mother Earth</b></font>
 <br>
 <font color="ADFF2F",size="12", face="Courier"><b>Since 2021</b></font>
-<br><br><br><br><br>
+<br><br><br><br><br><br>
 <font color="006400",size="2", face="Courier"><b>A Group 8 Project in TLE 9</b></font>
 
 </body>
