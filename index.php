c0a0c2531c75a982c174a4ae7cb5aee06ddc457c
Jerwen Kiev <77055176+mrjerwenkiev7@users.noreply.github.com>
2021-02-25 21:21:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   <img src="MATH - PERFORMANCE TASK DESIGN.png", width="400"></img>
   <br><br><br>
 <p align="justify">
-  <font color="ADFF2F", size="5", face="Courier">
+  <font color="ADFF2F", size="4", face="Courier">
     Losing opportunities, time, and chances discouraged us greatly. Losing someone we love was never easy. Losing things and jobs which sustain is will give us heavy burdens to carry. But losing hope will shatter our lives.
     <br><br>
     The pandemic may have dimmed all the lights that we were all trying to construct in hopes to see a brighter future, left us in pitch black, and devoured all the hope that we have, know that there will always be an intangible light inside you that only you can choose to dim... so have hope. In our artwork, you can see the cross at center and at the uppermost part because God is the intangible light- He is the one that will continue to shine within us in midst of the darkness, He is hope and trusting Him is the best way to see a bright side. 
@@ -38,6 +38,7 @@
     If we learn to lift everything and do the work by doing good. Great things will happen at the right time, at the right moment, at the right place, at the right person. In the middle of this pandemic, hope is important to persevere because it gives us courage to see the brighter side of every difficulties that come.  May this be a reminder for us to keep going no matter what we comes to hinder us. Let's be hopeful, always.
   </font>
   <br><br><br><br>
+<p align="center">
   <font color="006400", size="2", face="Courier"><b>A Group 8 Collaborative Project in TLE and MATH 9</b></font>
 
 </body>
