fbe62153467e1b250d472897f805fbea9ca77848
Jerwen Kiev <77055176+mrjerwenkiev7@users.noreply.github.com>
2021-02-26 09:53:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,9 +25,9 @@
     <br><br><br>
   <font color="006400", size="6", face="Courier"><b>Music Links:</b></font>
     <br>
-  <audio src="Masdan Mo Ang Kapaligiran(MP3_160K).mp3", type="audio/mp3">Asin - Masdan Mo Ang Kapaligiran</audio>
+  <audio src="Masdan Mo Ang Kapaligiran(MP3_160K).mp3", type="audio/mp3"></audio>
     <br>
-  <audio src="Alapaap(MP3_160K).mp3", type="audio/mp3">Eraserheads - Alapaap</audio>
+  <audio src="Alapaap(MP3_160K).mp3", type="audio/mp3"></audio>
     <br><br><br>
   <img src="MATH - PERFORMANCE TASK DESIGN.png", width="400"></img>
     <br><br><br>
