a6d0865b9fcbbf1fed18a430719f3225a5176387
Jerwen Kiev <77055176+mrjerwenkiev7@users.noreply.github.com>
2021-02-26 13:08:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
   <head>
     <title> Ecolution </title> 
     </head>
-
 <frameset cols="20%,80%", frameborder="NO">
  <frame src="Frame #1 (Final PT).html", noresize=“noresize”> 
  <frame src="Ecolution Homepage (Main).html", noresize=“noresize”>
-
 </frameset>
 </html>
