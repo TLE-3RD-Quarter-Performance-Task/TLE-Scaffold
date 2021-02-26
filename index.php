3702fae8f676f319b74c91fb8aa141cee34db47f
Jerwen Kiev <77055176+mrjerwenkiev7@users.noreply.github.com>
2021-02-26 09:12:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
   <head>
     <title> Ecolution </title> 
-  </head>
+    </head>
 
 <body> 
   <style> 
     body {background-image: url("week3/bg.jpg"); background-repeat: no-repeat; background-attachment: fixed; background-size: cover;}
-  </style>
+    </style>
   <br><br><br><br><br><br>
 <p align="center">
   <img src="week3/logo.png", width="210", length="210"></img>
@@ -21,7 +21,7 @@
   <br>
   <a href="https://youtu.be/MV4PNpAKGgc">
     <font color="ADFF2F", size="5", face="Courier"><b><i>(Click here)</i></b></font>
-  </a>
+    </a>
   <br><br><br>
   <img src="MATH - PERFORMANCE TASK DESIGN.png", width="400"></img>
   <br><br><br>
@@ -36,29 +36,28 @@
     COVID-19 pandemic affected the lives of many people, that's why through our artwork, we wanted relay the message, “Never lose hope.” Trust God for He is the center of everything, always let the Holy Spirit to bring you peace and hope and be your driving force to be a light to someone else. Hope is created at the intersections of faith, perseverance, and love. In this time of the pandemic, one of the greatest gifts you can give to those you love is the invitation to discover hope.
     <br><br>
     If we learn to lift everything and do the work by doing good, great things will happen at the right time, at the right moment, at the right place, and at the right person. In the middle of this pandemic, hope is important to persevere because it gives us courage to see the brighter side of every difficulties that come. May this be a reminder for us to keep going no matter what whatever shortcomings come to hinder us. Let's be hopeful, always.
-  </font>
+    </font>
   <br><br><br>
+<p align="center">
   <font color="006400", size="6", face="Courier"><b>OTHER VIDEO LINKS:</b></font>
   <br>
   <a href="https://youtu.be/8Z1eMy2FoX4">
     <font color="ADFF2F", size="5", face="Courier"><b><i>Alive | CANADA 4K</i></b></font>
-  </a>
+    </a>
   <br>
   <a href="https://youtu.be/yOWhEY9awc4">
     <font color="ADFF2F", size="5", face="Courier"><b><i>HOPE (Christian Inspirational & Motivational Video)</i></b></font>
-  </a>
+    </a>
   <br>
   <a href="https://youtu.be/rLY174n_KWA">
     <font color="ADFF2F", size="5", face="Courier"><b><i>COVID-19 FILM: A Message of Hope (Inspirational Video)</i></b></font>
-  </a>
+    </a>
   <br><br><br><br>
-<p align="center">
   <font color="006400", size="4", face="Courier">
     <b>
-      A Group 8 Project in TLE and MATH 9
-      <br>
+      A Group 8 Project in TLE and MATH 9<br>
       Copyright &#169 2021
-    </b>
+      </b>
   </font>
 
 </body>
